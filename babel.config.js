ac558ae70ff0919e51a2f7103d082fe0e44d923d
Josh Vasquez <Joshvasquez204@gmail.com>
2019-11-19 18:27:21 -0600

    added and fixed functionality

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function(api) {
+    api.cache(() => process.env.NODE_ENV);
+  
+    return {
+      presets: [
+        // require("@babel/preset-es2015"),
+        require("@babel/preset-env"),
+        require("@babel/preset-react")
+      ],
+      plugins: [
+        require("@babel/plugin-syntax-dynamic-import"),
+        [require("@babel/plugin-proposal-class-properties"), { loose: true }]
+      ]
+    };
+  };
---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const routes = require("./routes");
+
+// Passport configuration
+const db = require("../../config/connection");
+require("../../services/passport");
+
+// Mongoose Connection
+db(process.env.MONGODB_URI || "mongodb://localhost/project3");
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve Static
+app.use(express.static("dist"));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+  console.log(`Dev app url https://localhost:${PORT}/`);
+  console.log(`Dev api url https://localhost:${PORT}/`);
+});
